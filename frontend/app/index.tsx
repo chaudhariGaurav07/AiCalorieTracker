d13d989f7958c5fae4341b82d1bc379a3446570f
chaudhariGaurav07 <gauravchaudhari7717@gmail.com>
2025-07-03 16:03:30 +0530

    talwind setup is done

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
-
+import '../global.css'
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+     className="flex-1 items-center justify-center"
     >
-      <Text>Hii Gaurav.</Text>
+      <Text className="text-red-700">Hii Gaurav.</Text>
     </View>
   );
 }
